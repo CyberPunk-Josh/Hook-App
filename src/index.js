bc91032a23a4e35e078c6e10379e50cb2de65b75
CyberPunk-Josh <cristianjoshue@hotmail.com>
2022-03-21 22:43:44 -0600

    UseReducer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { CallbackHook } from './components/06-memos/CallbackHook';
+import { TodoApp } from './components/08-useReducer/TodoApp';
+// import { MemoHook } from './components/06-memos/MemoHook';
 // import { HookApp } from './HookApp';
 // import {CounterWithCustomHook} from './components/01-useState/CounterWithCustomHook';
 // import {SimpleForm} from './components/02-useEffect/SimpleForm';
@@ -7,9 +10,13 @@ import ReactDOM from 'react-dom';
 // import {MultipleCustomsHooks} from './components/03-examples/MultipleCustomsHooks';
 // import { FocusScreen } from './components/04-useRef/FocusScreen';
 // import { RealExampleRef } from './components/04-useRef/RealExampleRef';
-import { Layout } from './components/05-useLayout/Layout';
+// import { Layout } from './components/05-useLayout/Layout';
+// import { Memorize } from './components/06-memos/Memorize';
 
 ReactDOM.render(
-    <Layout/>,
+    <TodoApp/>,
   document.getElementById('root')
 );
+
+
+// import './components/08-useReducer/intro-reducer';
